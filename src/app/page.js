8b322f4d7e32c2cfaa286817c6961719afae7682
mitesh-24 <miteshhk05@gmail.com>
2023-12-26 17:17:31 +0530

    navbar added

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <>
-      <h1>Hello world !</h1>
-      <h2>This app is using app router. Search nextjs app router</h2>
-      <h3>Head over to /about and see the folder structure to get more info.</h3>
+      
     </>
   )
 }
