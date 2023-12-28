f463490dd96858b8e26569372caa86ea7f96ccd0
mitesh-24 <miteshhk05@gmail.com>
2023-12-28 18:32:37 +0530

    nav-bar done, website background done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Hero from "@/components/Hero";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Hero />
+    </div>
+  );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,41 +6,40 @@ import {
   IconButton,
   Typography,
   Button,
-  Stack,
-  Container,
   List,
   ListItem,
 } from "@mui/material";
 import TravelExploreIcon from "@mui/icons-material/TravelExplore";
-import Link from "@mui/material";
 import { useState } from "react";
-import CloseIcon from "@mui/icons-material/Close";
 import Drawer from "@mui/material/Drawer";
 import Divider from "@mui/material/Divider";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import FolderIcon from "@mui/icons-material/Folder";
-import ImageIcon from "@mui/icons-material/Image";
-import DescriptionIcon from "@mui/icons-material/Description";
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
 
 const navItems = [
-    {
-        text: "Gallery", link: "/gallery"
-    },
-    {
-        text: "Plans", link: "/plans"
-    },
-    {
-        text: "Destination", link: "/destination"
-    },
-    {
-        text: "About", link: "/about"
-    }
-]
+  {
+    text: "Gallery",
+    link: "/gallery",
+  },
+  {
+    text: "Plans",
+    link: "/plans",
+  },
+  {
+    text: "Destination",
+    link: "/destination",
+  },
+  {
+    text: "About",
+    link: "/about",
+  },
+];
+
+const navStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0.8)",
+  backdropFilter: "blur(5px)",
+};
 
 export default function Nav() {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -49,9 +48,7 @@ export default function Nav() {
     setMobileOpen((prevState) => !prevState);
   };
 
-
   const drawer = (
-
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
       <Typography variant="h6" sx={{ my: 2 }}>
         PHOTOWALK
@@ -69,61 +66,64 @@ export default function Nav() {
     </Box>
   );
 
-  const container = window !== undefined ? () => window.document.body : undefined;
-
+  const container =
+    typeof window !== "undefined" ? () => window.document.body : undefined;
 
   return (
     <Box sx={{ display: "flex" }}>
-      <AppBar component="nav" position="sticky" color="transparent" elevation={0}>
-          <Toolbar>
-            <IconButton>
-              <TravelExploreIcon />
-            </IconButton>
-            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-              PHOTOWALK
-            </Typography>
+      <AppBar
+        component="nav"
+        position="fixed" // Set position to fixed
+        color="transparent"
+        elevation={0}
+        style={{ ...navStyle, padding: 0 }} // Remove padding
+      >
+        <Toolbar>
+          <IconButton>
+            <TravelExploreIcon />
+          </IconButton>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            PHOTOWALK
+          </Typography>
 
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerToggle}
-              sx={{
-                mr: 2,
-                display: {
-                  xs: "block",
-                  sm: "none",
-                },
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerToggle}
+            sx={{
+              mr: 2,
+              display: {
+                xs: "block",
+                sm: "none",
+              },
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
 
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             {navItems.map((item) => (
-              <Button key={item.text}>
-                {item.text}
-              </Button>
+              <Button key={item.text}>{item.text}</Button>
             ))}
           </Box>
-          </Toolbar>
+        </Toolbar>
       </AppBar>
-          <nav>
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor="right"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
-            sx={{display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box' }}}
-            >
-                {drawer}
-            </Drawer>
-          </nav>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          anchor="right"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{ display: { xs: 'block', sm: 'none' }, '& .MuiDrawer-paper': { boxSizing: 'border-box' } }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
     </Box>
   );
 }
