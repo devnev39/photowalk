56c5c8a61fe0d3968dcefd8405d2e676c863e57f
Dhruv Bonde <devnev39@gmail.com>
2024-01-20 15:54:45 +0530

    refactor: refactored merged changed

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,25 +1,91 @@
-
-import { Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 const destinations = [
-  {id: 1, name: 'Kasba', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Mandai', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Camp', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'JM Road', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Flower Market', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'ARAI', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Swami Narayan', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'FC Road', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Pune Metro', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'ISKON Temple', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Aga Khan Palace', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Friendship Garden', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Ganpati Darshan', imageSrc:'/photographer.png', location:'/bird.png'},
-  {id: 1, name: 'Laxmi Road', imageSrc:'/photographer.png', location:'/bird.png'}
+  {
+    id: 1,
+    name: "Kasba",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "Mandai",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  { id: 1, name: "Camp", imageSrc: "/photographer.png", location: "/bird.png" },
+  {
+    id: 1,
+    name: "JM Road",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "Flower Market",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  { id: 1, name: "ARAI", imageSrc: "/photographer.png", location: "/bird.png" },
+  {
+    id: 1,
+    name: "Swami Narayan",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "FC Road",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "Pune Metro",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "ISKON Temple",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "Aga Khan Palace",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "Friendship Garden",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "Ganpati Darshan",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
+  {
+    id: 1,
+    name: "Laxmi Road",
+    imageSrc: "/photographer.png",
+    location: "/bird.png",
+  },
 ];
 
 const Destinations = () => {
@@ -34,27 +100,31 @@ const Destinations = () => {
 
   return (
     <Container sx={{ mt: 8, textAlign: "center" }}>
-      <Typography variant="h2" sx={{ color: "#333", fontWeight: "bold", mb: 2 }}>
+      <Typography
+        variant="h2"
+        sx={{ color: "#333", fontWeight: "bold", mb: 2 }}
+      >
         Destinations
       </Typography>
-      <Typography variant="subtitle1" sx={{ color: "#666", fontStyle: "italic", mb: 5 }}>
+      <Typography
+        variant="subtitle1"
+        sx={{ color: "#666", fontStyle: "italic", mb: 5 }}
+      >
         Where Every Picture Tells a Story
       </Typography>
       <Slider {...settings}>
         {destinations.map((destination) => (
-          <Card key={destination.id} style={{ margin: '10px' }}>
+          <Card key={destination.id} style={{ margin: "10px" }}>
             <CardActionArea>
               <Grid container spacing={2}>
-                
                 <Grid item md={6} xs={12}>
                   <CardMedia
                     component="img"
                     height="400"
                     image={destination.imageSrc}
                     alt={destination.name}
-                    style={{ position: 'relative' }}
+                    style={{ position: "relative" }}
                   />
-                  
                 </Grid>
                 <Grid item md={6} xs={12}>
                   <iframe
@@ -70,11 +140,20 @@ const Destinations = () => {
                   ></iframe>
                 </Grid>
               </Grid>
-              <CardContent sx={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.7)', color: 'white', padding: '10px' }}>
-                  <Typography variant="h5" component="div">
-                    {destination.name}
-                  </Typography>
-                </CardContent>
+              <CardContent
+                sx={{
+                  position: "absolute",
+                  bottom: 0,
+                  width: "100%",
+                  backgroundColor: "rgba(0, 0, 0, 0.7)",
+                  color: "white",
+                  padding: "10px",
+                }}
+              >
+                <Typography variant="h5" component="div">
+                  {destination.name}
+                </Typography>
+              </CardContent>
             </CardActionArea>
           </Card>
         ))}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,30 @@
 "use client";
 
-import { AppBar, Container, Toolbar, Typography, Link } from "@mui/material";
+import { AppBar, Toolbar, Typography, Link } from "@mui/material";
 
 const Footer = () => {
   return (
-    
-      <AppBar position="static" color="primary" sx={{mt:'auto'}}>
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <div>
-            <Typography variant="body2" color='inherit'>
-              © {new Date().getFullYear()} Photowalk. All rights reserved.
-            </Typography>
-          </div>
-          <div>
-            <Link href="/privacy-policy" color="inherit" sx={{ mx: 1 }}>
-              Privacy Policy
-            </Link>
-            <Link href="/terms-of-service" color="inherit" sx={{ mx: 1 }}>
-              Terms of Service
-            </Link>
-            <Link href="/contact-us" color="inherit" sx={{ mx: 1 }}>
-              Contact Us
-            </Link>
-          </div>
-        </Toolbar>
-      </AppBar>
-   
+    <AppBar position="static" color="primary" sx={{ mt: "auto" }}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <div>
+          <Typography variant="body2" color="inherit">
+            © {new Date().getFullYear()} Photowalk. All rights reserved.
+          </Typography>
+        </div>
+        <div>
+          <Link href="/privacy-policy" color="inherit" sx={{ mx: 1 }}>
+            Privacy Policy
+          </Link>
+          <Link href="/terms-of-service" color="inherit" sx={{ mx: 1 }}>
+            Terms of Service
+          </Link>
+          <Link href="/contact-us" color="inherit" sx={{ mx: 1 }}>
+            Contact Us
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
 export default Footer;
