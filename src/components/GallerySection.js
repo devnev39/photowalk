537bcad5baad3335a581fa3805e1c7f4860b54aa
mitesh-24 <miteshhk05@gmail.com>
2024-01-21 18:50:00 +0530

    refactor: added inline css for galllerysection

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,9 +1,35 @@
 "use client";
 
+import { useState } from "react";
 import { Container, Grid, Typography } from "@mui/material";
-import "../app/gallerystyles.css";
 
 const GallerySection = () => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleImageHover = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleImageLeave = () => {
+    setHoveredIndex(null);
+  };
+
+  const galleryImages = [
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+    "/download.jpg",
+  ];
+
   return (
     <Container sx={{ mt: 8, textAlign: "center" }}>
       <Grid container spacing={1} sx={{ textAlign: "center", mt: 8, mb: 5 }}>
@@ -25,29 +51,37 @@ const GallerySection = () => {
         <Grid
           item
           lg
-          className="container"
           sx={{
             justifyContent: "center",
             alignItems: "center",
             display: "flex",
             flexWrap: "wrap",
+            mx: 10,
+            mt: 5,
+            width: "80vw",
+            height: "50vmin",
+            gap: "1vmin",
           }}
         >
-          <Grid item lg className="container" sx={{ mx: 10, mt: 5 }}>
-            <img className="gallery-image" src="/download.jpg" alt="Image 1" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 2" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-            <img className="gallery-image" src="/download.jpg" alt="Image 3" />
-          </Grid>
+          {galleryImages.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Image ${index + 1}`}
+              style={{
+                height: "100%",
+                filter: `grayscale(${hoveredIndex === index ? 0 : 1}) brightness(${
+                  hoveredIndex === index ? 1.15 : 0.75
+                })`,
+                objectFit: "cover",
+                overflow: "hidden",
+                flex: hoveredIndex === index ? 6 : 1,
+                transition: "flex 0.5s, filter 0.5s",
+              }}
+              onMouseEnter={() => handleImageHover(index)}
+              onMouseLeave={handleImageLeave}
+            />
+          ))}
         </Grid>
       </Grid>
     </Container>
