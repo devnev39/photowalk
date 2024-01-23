51c8a77eaa69588a29a8e8ab3b31fa62be501551
Dhruv Bonde <devnev39@gmail.com>
2024-01-23 23:49:49 +0530

    fix: added new key in user shcema

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,13 @@
 export const userSchema = {
-    email: "",
-    role: "",
-    can_createuser: false,
-    can_createplan: false,
-    can_updateplan: false,
-    is_setupcompleted: false,
-    created_at: "",
-    created_by: "",
-    updated_by: "",
-    updated_at: ""
-}
+  email: "",
+  role: "",
+  can_createuser: false,
+  can_createplan: false,
+  can_updateplan: false,
+  can_uploadimage: false,
+  is_setupcompleted: false,
+  created_at: "",
+  created_by: "",
+  updated_by: "",
+  updated_at: "",
+};
