824099660ee2e32d5cb0665689d3aff8cde18793
Dhruv Bonde <devnev39@gmail.com>
2024-01-23 23:28:23 +0530

    feat: added production config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,30 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const dev = process.env.NEXT_PUBLIC_ENV === "development";
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-  appId: process.env.NEXT_PUBLIC_APPID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
+  apiKey: dev
+    ? process.env.NEXT_PUBLIC_APIKEY
+    : process.env.NEXT_PUBLIC_APIKEY_PRD,
+  authDomain: dev
+    ? process.env.NEXT_PUBLIC_AUTHDOMAIN
+    : process.env.NEXT_PUBLIC_AUTHDOMAIN_PRD,
+  projectId: dev
+    ? process.env.NEXT_PUBLIC_PROJECTID
+    : process.env.NEXT_PUBLIC_PROJECTID_PRD,
+  storageBucket: dev
+    ? process.env.NEXT_PUBLIC_STORAGEBUCKET
+    : process.env.NEXT_PUBLIC_STORAGEBUCKET_PRD,
+  messagingSenderId: dev
+    ? process.env.NEXT_PUBLIC_MESSAGINGSENDERID
+    : process.env.NEXT_PUBLIC_MESSAGINGSENDERID_PRD,
+  appId: dev
+    ? process.env.NEXT_PUBLIC_APPID
+    : process.env.NEXT_PUBLIC_APPID_PRD,
+  measurementId: dev
+    ? process.env.NEXT_PUBLIC_MEASUREMENTID
+    : process.env.NEXT_PUBLIC_MEASUREMENTID_PRD,
 };
 
 const app = initializeApp(firebaseConfig);
